ed70c6305e7787d50d26172e3002c3dbe6a59b45
victorjudysen <117171668+victorjudysen@users.noreply.github.com>
2024-05-23 14:15:54 +0300

    changes_v5

--- arbitrage.cpp
+++ arbitrage.cpp
@@ -31,27 +31,24 @@ int main() {
 
     sumOfProbabilities = probabilityOfTeamA + probabilityOfTeamB;
     int sumIntoPercent = (sumOfProbabilities) * 100;
-    cout << "Summation of Probabilities is: " << sumOfProbabilities << ", "<< "In percentage is: "<< sumIntoPercent << "/100"<< endl;
+    cout << "Probability Summation is: " << sumOfProbabilities << " == " << "["<< sumIntoPercent << "%" << "]"<< endl;
 
     cout << endl;
     if (sumIntoPercent < 100) {
-        cout << "How much are you staking today?: ";
+        cout << "How much are you staking today? --> ";
         cin >> stakeAmount;
 
         cout << endl;
-        
         // Stake on Each
         stakeOnTeamA = (probabilityOfTeamB / sumOfProbabilities) * stakeAmount;
-        cout << "The stake on Team A is: " << stakeOnTeamA << endl;
-
+        cout << "The Stake on Team A is: " << stakeOnTeamA << endl;
         stakeOnTeamB = (probabilityOfTeamA / sumOfProbabilities) * stakeAmount;
-        cout << "The stake on Team B is: " << stakeOnTeamB << endl;
+        cout << "The Stake on Team B is: " << stakeOnTeamB << endl;
 
         cout << endl;
         // Payout on Each
         payoutForTeamA = (stakeOnTeamA * oddsOnteamA);
         cout << "The Payout on Team A is: " << payoutForTeamA << endl;
-
         payoutForTeamB = (stakeOnTeamA * oddsOnteamB);
         cout << "The Payout on Team B is: " << payoutForTeamB << endl;
 
@@ -65,7 +62,8 @@ int main() {
         cout << "If Team B wins, the profit is: " << profitForTeamB << endl;
     } 
     else {
-        cout << "This match has no arbitrage opportunity!!";
+        cout << "This match has no arbitrage opportunity, Please do choose another match and Try again!!";
+        cout << endl;
     }
     
     return 0;
